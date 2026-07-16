6b57c45ea8fe41785c323fb27fd3ca1718f671c7
Andy Marks <122892070+andymarksonline@users.noreply.github.com>
2026-07-16 11:52:54 +0100

    Bump version to 2.0.0 and fix issues.

--- Radzen.Blazor/RadzenTheme.razor.cs
+++ Radzen.Blazor/RadzenTheme.razor.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 #nullable enable
